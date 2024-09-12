e3e2486f200955b59c75c7b3ef2cccb3e4e31be9
Nathan Toews <nathantoews@AR-MB-02.bimm.internal>
2024-09-12 15:39:00 -0400

    fix: added safe directory flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,20 +23,23 @@ else
 fi
 
 git config --global --add safe.directory /github/workspace
+git config --global user.email "$INPUT_USER_EMAIL"
+git config --global user.name "$INPUT_USER_NAME"
+
+CLONE_DIR=$(mktemp -d)
 
 if [ -z `git branch --list $OUTPUT_BRANCH` ]
 then
   echo "Creating new branch: ${INPUT_DESTINATION_BRANCH}"
+  git clone --single-branch --branch main "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
   git checkout -b "$INPUT_DESTINATION_BRANCH"
   OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
 fi
 
-CLONE_DIR=$(mktemp -d)
-
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch $OUTPUT_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 if [ ! -z "$INPUT_RENAME" ]
 then
